8ec171eecfdc9e65c137002069fefb042b3fbfc8
Vaisakh Murali <mvaisakh@statixos.com>
2023-01-13 17:23:02 +0600

    sm8250-common: use vendor/hardware/xiaomi
    Change-Id: I31e88cdf8cd7cbaf320f4daf63290a8ed32619f0
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- kona.mk
+++ kona.mk
@@ -423,7 +423,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi \
+    vendor/hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
 # Telephony
