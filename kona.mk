c26e24f5aa9434008df4d705b6a40d414f28ae31
Vaisakh Murali <mvaisakh@statixos.com>
2023-01-30 12:33:23 +0000

    sm8250-common: drop QTI vibrator HAL
    Change-Id: I843b93cd6c2533c3141dee57aa85ea4dd30c1f92
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- kona.mk
+++ kona.mk
@@ -463,13 +463,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_kona
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
-
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
